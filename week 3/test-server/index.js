e63f8dbb134a0ca41cae0bd2bbb81fd64020a5b7
Jim Shaw <jimxshaw@gmail.com>
2016-01-29 13:59:57 -0600

    added index.htm, incorporated file upload functionality to sample web server

--- week 3/test-server/index.js
+++ week 3/test-server/index.js
@@ -1,18 +1,46 @@
 var express = require('express');
 var app = express();
+var fs = require("fs");
+var bodyParser = require('body-parser');
 
-app.get('/', function (req, res) {
-   console.log(req.url);
-   console.log(req.method);
-   res.send('Hello world!');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(multer({ dest: '/tmp/'}));
+var multer  = require('multer');
+var upload = multer({dest: './uploads/'});
+
+app.get('/index.htm', function (req, res) {
+   res.sendFile( __dirname + "/" + "index.htm" );
 });
 
-app.get('/helloJim', function (req, res) {
-   console.log(req.url);
-   console.log(req.method);
-   res.send('Hello Jim!');
+app.post('/file_upload', upload.single('photo'), function (req, res) {
+
+   console.log(req.file.name);
+   console.log(req.file.path);
+   console.log(req.file.type);
+
+   var file = __dirname + "/" + req.file.name;
+   fs.readFile( req.file.path, function (err, data) {
+        fs.writeFile(file, data, function (err) {
+         if( err ){
+              console.log( err );
+         }else{
+               response = {
+                   message:'File uploaded successfully',
+                   filename:req.file.name
+              };
+          }
+          console.log( response );
+          res.end( JSON.stringify( response ) );
+       });
+   });
 });
 
 var server = app.listen(8081, function () {
-  console.log("Example app listening at port 8081");
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log("Example app listening at http://%s:%s", host, port);
+
 });
